60c8affefebf46d98e24abda5a6a3d255801d102
Yonel <57262211+Mukute@users.noreply.github.com>
2020-11-24 14:12:41 +0300

    Initial commit

---
+++ component/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Python</li>
+      <li>JavaScript</li>
+      <li>C++</li>
+    </ul>
+  );
+}
+
+export default List;
